3fe1bee42e744e199c26a3557e81656f6d9c3da3
Валерия Михайлова <valeriya23.04@outlook.com>
2022-12-10 17:53:57 +0300

    Добавьте файлы проекта.

---
+++ labwork15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        StringBuilder sb = new StringBuilder();
+        foreach (char c in s)
+            sb.Append(char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c));
+        Console.WriteLine(sb.ToString());
+        Console.ReadKey();
+    }
+}
