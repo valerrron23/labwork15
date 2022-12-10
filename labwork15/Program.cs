92f1d4c61783675b1d7354dce782caa3eebc83fc
Михайлова Валерия Романовна <114227035+valerrron23@users.noreply.github.com>
2022-12-10 18:01:16 +0300

    Update Program.cs

--- labwork15/Program.cs
+++ labwork15/Program.cs
@@ -5,11 +5,11 @@ class Program
 {
     static void Main()
     {
-        string s = Console.ReadLine();
-        StringBuilder sb = new StringBuilder();
+        string s = Console.ReadLine(); //введите латинские маленькие буквы
+        StringBuilder sb = new StringBuilder(); 
         foreach (char c in s)
-            sb.Append(char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c));
-        Console.WriteLine(sb.ToString());
-        Console.ReadKey();
+            sb.Append(char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c)); //программа вычисляет по формуле, заменяя маленькие буквы на большие
+        Console.WriteLine(sb.ToString()); //вывод на консоль
+        Console.ReadKey(); //программа ожидает нажатия клавиши для завершения работы
     }
 }
